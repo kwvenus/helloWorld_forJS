4bd4ab0e3ca0d097af66a9c3451b9dba39aac8e8
Wong Cho Hing <venus.wong@oocl.com>
2018-11-21 00:44:09 +0800

    Unit test 4: prepareMultiplicationTable

--- __tests__/multiplicationTable-spec.js
+++ __tests__/multiplicationTable-spec.js
@@ -6,6 +6,8 @@ const wrongEndNumber = 2;
 const startNumber = 2;
 const endNumber = 4;
 
+const multiplicationCombination = multiplicationTable.findMultiplicationCombination(startNumber, endNumber);
+
 it('Given 2 integers, when start number is smaller then or equal to end number (isStartNumberSmallerThenOrEqualToEndNumber), then return true, otherwise return false', function(){
     expect(multiplicationTable.isStartNumberSmallerThenOrEqualToEndNumber(startNumber, endNumber)).toBe(true);
     expect(multiplicationTable.isStartNumberSmallerThenOrEqualToEndNumber(wrongStartNumber, wrongEndNumber)).toBe(false);
@@ -16,7 +18,7 @@ it('Given 2 valid integers, when both integers are with in the range of 1-1000 i
     expect(multiplicationTable.areNumberWithInTheRange(wrongStartNumber, wrongEndNumber)).toBe(false);
 });
 
-it('Give 2 valid integers within the range of 1-1000 inclusively, when findMultiplicationCombination, then return multiplicationCombination[combination{startCombinationNumber:int, endCombinationNumber:int}].', function(){
+it('Give 2 valid integers within the range of 1-1000 inclusively, when findMultiplicationCombination, then return multiplicationCombination[Object], where Object{startCombinationNumber:int, endCombinationNumber:int}].', function(){
     expect(multiplicationTable.findMultiplicationCombination(2, 4)).toEqual([
     {startCombinationNumber: 2, endCombinationNumber: 2},
     {startCombinationNumber: 2, endCombinationNumber: 3},
@@ -24,4 +26,11 @@ it('Give 2 valid integers within the range of 1-1000 inclusively, when findMulti
     {startCombinationNumber: 2, endCombinationNumber: 4},
     {startCombinationNumber: 3, endCombinationNumber: 4},
     {startCombinationNumber: 4, endCombinationNumber: 4}]);
+});
+
+it('Give 2 valid integers within the range of 1-1000 inclusively and multiplicationCombination, when prepareMultiplicationTable, then return multiplcation table.].', function(){
+    expect(multiplicationTable.prepareMultiplicationTable(multiplicationCombination)).toBe(
+    "2*2=4  \n" +
+    "2*3=6  3*3=9  \n" +
+    "2*4=8  3*4=12  4*4=16  ");
 });
--- multiplicationTable.js
+++ multiplicationTable.js
@@ -31,8 +31,38 @@ function findMultiplicationCombination(startNumber, endNumber){
     return multiplicationCombination;
 }
 
+function alignSpaceCount(startNumber, endNumber){
+    const baseLength = 2;
+    let numberLength = startNumber.toString().length + endNumber.toString().length;
+    let ansNumberLength = (startNumber*endNumber).toString.length;
+
+    return baseLength + numberLength + ansNumberLength;
+}
+
+function prepareMultiplicationTable(multiplicationCombination){
+    let i, j;
+    let ans = '';
+    let tempSpace = '';
+    let lastElement = multiplicationCombination[(multiplicationCombination.length)-1];
+    const finalSpace = alignSpaceCount(lastElement.startCombinationNumber, lastElement.endCombinationNumber);
+
+    for (i = 0; i < multiplicationCombination.length; i++){
+        ans += multiplicationCombination[i].startCombinationNumber + "*" + multiplicationCombination[i].endCombinationNumber + "=" + (multiplicationCombination[i].endCombinationNumber * multiplicationCombination[i].startCombinationNumber);
+
+            tempSpace = alignSpaceCount(multiplicationCombination[i].startCombinationNumber, multiplicationCombination[i].endCombinationNumber);
+            for (j = 0; j <= (finalSpace-tempSpace+1); j++){
+                ans += ' ';
+            }
+        if (i != (multiplicationCombination.length)-1 && multiplicationCombination[i].startCombinationNumber == multiplicationCombination[i].endCombinationNumber)
+            ans += "\n";
+    }
+    return ans;
+}
+
+
 module.exports = {
     isStartNumberSmallerThenOrEqualToEndNumber,
     areNumberWithInTheRange,
-    findMultiplicationCombination
+    findMultiplicationCombination,
+    prepareMultiplicationTable
 };
